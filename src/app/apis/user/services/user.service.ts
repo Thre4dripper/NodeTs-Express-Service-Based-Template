152d11ccaf0bc7831b6dd523ba5e3b6f7acfbd6a
Ijlal Ahmad <ijlalahmad845@gmail.com>
2024-01-07 18:28:56 +0530

    added bcrypt for password encryption

--- src/app/apis/user/services/user.service.ts
+++ src/app/apis/user/services/user.service.ts
@@ -1,6 +1,7 @@
 import { ILoginUser, IRegisterUser } from '../interfaces'
 import userRepository from '../repositories/user.repository'
 import { ValidationError } from '../../../handlers/CustomErrorHandler'
+import EncryptionUtil from '../../../utils/EncryptionUtil'
 
 class UserService {
     async registerUser(data: IRegisterUser) {
@@ -10,6 +11,8 @@ class UserService {
             throw new ValidationError('User already exists')
         }
 
+        data.password = await EncryptionUtil.hashPassword(data.password)
+
         return userRepository.create(data)
     }
 
@@ -18,6 +21,12 @@ class UserService {
 
         if (!user) {
             throw new ValidationError('User does not exist')
+        }
+
+        const isPasswordValid = await EncryptionUtil.comparePassword(data.password, user.password)
+
+        if (!isPasswordValid) {
+            throw new ValidationError('Invalid password')
         }
 
         return user
